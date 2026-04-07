e79774fd5becf02f72e0b25dbe3539d8d8ca1a1b
Marco van Beusichem <marcovanbeusichem@MacBook-Pro-van-Macbook.local>
2026-04-07 19:21:15 +0200

    Fix chromium defaultViewport type error

--- src/app/api/visits/[id]/pdf/route.ts
+++ src/app/api/visits/[id]/pdf/route.ts
@@ -39,9 +39,9 @@ export async function GET(
 
   const browser = await puppeteer.launch({
     args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
+    defaultViewport: { width: 1280, height: 900 },
     executablePath: await chromium.executablePath(),
-    headless: chromium.headless,
+    headless: true,
   })
 
   try {
